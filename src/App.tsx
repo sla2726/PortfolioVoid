a193020de83364fe343e86d8b06a8a2d0b40413f
sla2726 <fabiodasilvajuniosilva@gmail.com>
2025-07-21 21:31:13 +0000

    glitch automatic

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import './App.css';
+import { Terminal } from 'lucide-react';
 
 const chars = 'abcdefghijklmnopqrstuvwxyz0123456789@#*!?█▓▒░▀▄▌▐■□▪▫';
 
@@ -20,6 +21,20 @@ function glitchText(text: string) {
 export default function App() {
   const text = 'Void';
   const [displayText, setDisplayText] = useState(text);
+  const [glitchOn, setGlitchOn] = useState(false);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setGlitchOn((prev) => !prev);
+      setDisplayText(glitchText(text));
+
+      setTimeout(() => {
+        setDisplayText(text);
+        setGlitchOn(false);
+      }, 250);
+    }, 1000);
+    return (): void => clearInterval(interval);
+  });
 
   const handleGlitchText = (): void => {
     setDisplayText(glitchText(text));
@@ -28,11 +43,13 @@ export default function App() {
 
   return (
     <main className="min-h-screen w-screen bg-slate-700">
-      <div className="group relative inline-block text-2xl font-bold">
-        <div className="pointer-events-none inset-0 opacity-0 transition-opacity duration-200 select-none group-hover:opacity-100">
+      <div className="relative inline-block text-2xl font-bold">
+        <div
+          className={`pointer-events-none absolute inset-0 transition-opacity duration-200 select-none ${glitchOn ? 'opacity-100' : 'opacity-0'}`}>
           <h1 className="absolute top-0 left-0 translate-x-[2px] text-blue-500">{displayText}</h1>
           <h1 className="absolute top-0 left-0 -translate-x-[2px] text-red-500">{displayText}</h1>
         </div>
+
         <h1
           onMouseEnter={handleGlitchText}
           onTouchStart={handleGlitchText}
