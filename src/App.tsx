34d0b904b9c97a689a706e4da230da1295e3d6a1
sla2726 <fabiodasilvajuniosilva@gmail.com>
2025-07-21 21:00:52 +0000

    glitchEffect

--- src/App.tsx
+++ src/App.tsx
@@ -25,13 +25,21 @@ export default function App() {
     setDisplayText(glitchText(text));
     setTimeout(() => setDisplayText(text), 300);
   };
+
   return (
     <main className="min-h-screen w-screen bg-slate-700">
-      <header className="text-2xl">
-        <h1 onMouseEnter={handleGlitchText} onTouchStart={handleGlitchText}>
+      <div className="group relative inline-block text-2xl font-bold">
+        <div className="pointer-events-none inset-0 opacity-0 transition-opacity duration-200 select-none group-hover:opacity-100">
+          <h1 className="absolute top-0 left-0 translate-x-[2px] text-blue-500">{displayText}</h1>
+          <h1 className="absolute top-0 left-0 -translate-x-[2px] text-red-500">{displayText}</h1>
+        </div>
+        <h1
+          onMouseEnter={handleGlitchText}
+          onTouchStart={handleGlitchText}
+          className="relative z-10">
           {displayText}
         </h1>
-      </header>
+      </div>
     </main>
   );
 }
